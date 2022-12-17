0747537a7aa4be5fe2fc616a8603000876600497
Jubayer Juhan <davidjuhan23@gmail.com>
2022-12-17 17:06:11 +0600

    Server Link Changed

--- src/client.js
+++ src/client.js
@@ -5,25 +5,25 @@ const jwtToken = getLocalStorageData("jwtToken");
 const adminJwtToken = getLocalStorageData("adminJwtToken");
 
 export const client = axios.create({
-  // baseURL:
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://my-instructor-server.ts.r.appspot.com/api"
-  //     : "http://localhost:5000/api",
-  baseURL: "https://api.myinstructor.com.au/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://my-instructor-server.ts.r.appspot.com/api"
+      : "http://localhost:5000/api",
+  // baseURL: "https://api.myinstructor.com.au/api",
   headers: {
     authorization: `Bearer ${jwtToken}`,
   },
 });
 
 export const admin = axios.create({
-  // baseURL:
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://my-instructor-server.ts.r.appspot.com/api/admin"
-  //     : "http://localhost:5000/api/admin",
-  baseURL: "https://api.myinstructor.com.au/api/admin",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://my-instructor-server.ts.r.appspot.com/api/admin"
+      : "http://localhost:5000/api/admin",
+  // baseURL: "https://api.myinstructor.com.au/api/admin",
   headers: {
     authorization: `Bearer ${adminJwtToken}`,
   },
 });
 
-export const WEBSOCKET_URL = "wss://api.myinstructor.com.au";
+export const WEBSOCKET_URL = "wss://my-instructor-server.ts.r.appspot.com";
