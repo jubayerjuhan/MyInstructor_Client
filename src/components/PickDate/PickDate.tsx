cfb250871fa144e1d8a51a7da5380fe7669e6346
Jubayer Juhan <davidjuhan23@gmail.com>
2023-05-03 02:33:44 +0600

    Financial Report list mobile responsive

--- src/components/PickDate/PickDate.tsx
+++ src/components/PickDate/PickDate.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 interface Props {
@@ -10,9 +8,7 @@ interface Props {
 const PickDate = ({ title, onChange }: Props) => {
   return (
     <div>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker label={title} onChange={(val) => onChange(val)} />
-      </LocalizationProvider>
+      <DatePicker label={title} onChange={(val) => onChange(val)} />
     </div>
   );
 };
--- src/pages/FinancialReporting/FinancialReporting.tsx
+++ src/pages/FinancialReporting/FinancialReporting.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import moment from "moment";
 import { Button } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { useDispatch, useSelector } from "react-redux";
 
 import DatePickerComponent from "../../components/PickDate/PickDate";
@@ -55,8 +57,10 @@ const FinancialReporting = () => {
       <h4 className="header">Financial Reports</h4>
       <p className="description">Select a start and end date to view financial reports between the chosen period.</p>
       <div className="date__selector">
-        <DatePickerComponent title="From" onChange={(value) => handleChange("from", value)} />
-        <DatePickerComponent title="To" onChange={(value) => handleChange("to", value)} />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePickerComponent title="From" onChange={(value) => handleChange("from", value)} />
+          <DatePickerComponent title="To" onChange={(value) => handleChange("to", value)} />
+        </LocalizationProvider>
         {period?.from && period.to && (
           <Button variant="contained" onClick={handleFilter}>
             Filter
